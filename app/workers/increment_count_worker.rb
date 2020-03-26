2d3ea85e68d58ba551e5ac3d102c9c03b78738e6
baodang <baodq96@gmail.com>
2020-03-26 09:05:11 +0700

    add sidekiq & redis

---
+++ app/workers/increment_count_worker.rb
@@ -0,0 +1,12 @@
+class IncrementCountWorker
+  include Sidekiq::Worker
+
+  def perform(post_id)
+    # Do something
+    post = Post.find(post_id)
+
+    post.likes_count ||= 0
+    post.likes_count += 1
+    post.save
+  end
+end
